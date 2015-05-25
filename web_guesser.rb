8365d4bc369a9667ddaceddf1f4a4bcedb5b3511
Leah Garber <ldgarber@users.noreply.github.com>
2015-05-24 21:47:24 -0400

    Added styling and fixed warning

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,30 +1,24 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-@@secret_number = rand(100)
-@@guesses = 5
+class Guesser
+    @@secret_number = rand(100)
+    @@guesses = 5
 
-get '/' do
-    guess = params[:guess]
-    cheat = params[:cheat]
-    message2 = ""
-    if guess
-        message = check_guess(guess)
-        color = assign_color(message)
-        message2 = adjust_guesses(guess)
-    else
-        @@guesses = 5
-        @@secret_number = rand(100)
-        message = "Guess the number from 0-100 in 5 guesses!"
-        color = "#FFFFFF"
-    end
-    if cheat
-        cheatmessage = "(Psst. It's #{@@secret_number})"
-    else
-        cheatmessage = ""
-    end
-    erb :index, :locals => {:message => message, :color => color, :message2 => message2, :cheatmessage => cheatmessage}
+def secret_number
+    @@secret_number
+end
 
+def guesses=(guesses)
+    @@guesses = guesses
+end
+
+def self.guesses
+    @@guesses
+end
+
+def reset_secret_number
+    @@secret_number = rand(100)
 end
 
 def check_guess(guess)
@@ -42,7 +36,7 @@ def check_guess(guess)
             message = "Too low!"
         end
     else
-        message = "You got it right! \n The SECRET NUMBER is #{@@secret_number}"
+        message = "You got it right! \n The SECRET NUMBER is #{@@secret_number}."
     end
 
     return message
@@ -53,7 +47,7 @@ def adjust_guesses(guess)
     if guess != @@secret_number
         @@guesses -= 1
         if @@guesses == 0
-            message2 = "Out of guesses, generating a new number"
+            message2 = "Out of guesses, generating a new number."
             @@secret_number = rand(100)
             @@guesses = 5
         else
@@ -62,18 +56,43 @@ def adjust_guesses(guess)
     else
         @@guesses = 5
         @@secret_number = rand(100)
-        message2 = "#{@@guesses} guesses remaining to guess new number"
+        message2 = "#{@@guesses} guesses remaining to guess new number."
     end
     return message2
 end
 
 def assign_color(message)
     if message == "Way too high!" || message == "Way too low!"
-        color = "#DB3535"
+        color = "#DB3548"
     elsif message == "Too high!" ||  message == "Too low!"
         color = "#FF8C8C"
     else
         color = "#33CC33"
     end
     return color
+end
+end
+
+wg = Guesser.new
+get '/' do
+    guess = params[:guess]
+    cheat = params[:cheat]
+    message2 = ""
+    if guess
+        message = wg.check_guess(guess)
+        color = wg.assign_color(message)
+        message2 = wg.adjust_guesses(guess)
+    else
+        wg.guesses = 5
+        wg.reset_secret_number
+        message = "Guess the number from 0-100 in 5 guesses!"
+        color = "#35DBC8"
+    end
+    if cheat
+        cheatmessage = "(Psst. It's #{wg.secret_number})"
+    else
+        cheatmessage = ""
+    end
+    erb :index, :locals => {:message => message, :color => color, :message2 => message2, :cheatmessage => cheatmessage}
+
 end
