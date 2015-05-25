02c74e9abc5da277c6bc65f0fb95f238a5a76787
Leah Garber <ldgarber@users.noreply.github.com>
2015-05-24 21:57:29 -0400

    fixed indentation

--- web_guesser.rb
+++ web_guesser.rb
@@ -5,72 +5,72 @@ class Guesser
     @@secret_number = rand(100)
     @@guesses = 5
 
-def secret_number
-    @@secret_number
-end
-
-def guesses=(guesses)
-    @@guesses = guesses
-end
-
-def self.guesses
-    @@guesses
-end
-
-def reset_secret_number
-    @@secret_number = rand(100)
-end
-
-def check_guess(guess)
-    guess = guess.to_i
-    if guess > @@secret_number
-        if guess - @@secret_number > 5
-            message = "Way too high!"
-        else
-            message = "Too high!"
-        end
-    elsif guess < @@secret_number
-        if @@secret_number - guess > 5
-            message =  "Way too low!"
-        else
-            message = "Too low!"
-        end
-    else
-        message = "You got it right! \n The SECRET NUMBER is #{@@secret_number}."
+    def secret_number
+        @@secret_number
     end
 
-    return message
-end
+    def guesses=(guesses)
+        @@guesses = guesses
+    end
 
-def adjust_guesses(guess)
-    guess = guess.to_i
-    if guess != @@secret_number
-        @@guesses -= 1
-        if @@guesses == 0
-            message2 = "Out of guesses, generating a new number."
-            @@secret_number = rand(100)
-            @@guesses = 5
-        else
-            message2 = "#{@@guesses} guesses remaining"
-        end
-    else
-        @@guesses = 5
+    def self.guesses
+        @@guesses
+    end
+
+    def reset_secret_number
         @@secret_number = rand(100)
-        message2 = "#{@@guesses} guesses remaining to guess new number."
     end
-    return message2
-end
 
-def assign_color(message)
-    if message == "Way too high!" || message == "Way too low!"
-        color = "#DB3548"
-    elsif message == "Too high!" ||  message == "Too low!"
-        color = "#FF8C8C"
-    else
-        color = "#33CC33"
+    def check_guess(guess)
+        guess = guess.to_i
+        if guess > @@secret_number
+            if guess - @@secret_number > 5
+                message = "Way too high!"
+            else
+                message = "Too high!"
+            end
+        elsif guess < @@secret_number
+            if @@secret_number - guess > 5
+                message =  "Way too low!"
+            else
+                message = "Too low!"
+            end
+        else
+            message = "You got it right! \n The SECRET NUMBER is #{@@secret_number}."
+        end
+
+        return message
     end
-    return color
-end
+
+    def adjust_guesses(guess)
+        guess = guess.to_i
+        if guess != @@secret_number
+            @@guesses -= 1
+            if @@guesses == 0
+                message2 = "Out of guesses, generating a new number."
+                @@secret_number = rand(100)
+                @@guesses = 5
+            else
+                message2 = "#{@@guesses} guesses remaining"
+            end
+        else
+            @@guesses = 5
+            @@secret_number = rand(100)
+            message2 = "#{@@guesses} guesses remaining to guess new number."
+        end
+        return message2
+    end
+
+    def assign_color(message)
+        if message == "Way too high!" || message == "Way too low!"
+            color = "#DB3548"
+        elsif message == "Too high!" ||  message == "Too low!"
+            color = "#FF8C8C"
+        else
+            color = "#33CC33"
+        end
+        return color
+    end
 end
 
 wg = Guesser.new
